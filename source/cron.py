6de39e6f9aae5751c4565cbd7c30dbb9c5ed2da2
2eey10 <coherentman0103@naver.com>
2024-01-09 01:22:12 +0900

    1차

---
+++ source/cron.py
@@ -0,0 +1,15 @@
+from croniter import croniter
+from crontab import CronTab
+
+cron_expression = '*/30 * * * *'
+
+cron = CronTab(user="ec2-user")
+
+for job in cron:
+    if job.setall(cron_expression):
+        cron.remove(job)
+
+new_job = cron.new(command='/bin/bash -c "sleep 30 && docker exec biscon /usr/bin/python3 /bis/source/test.py"') 
+new_job.setall(cron_expression)
+
+cron.write()
