ec4f85e5f0abad8a13644629648bda88417817e1
2eey10 <coherentman0103@naver.com>
2024-01-10 00:19:53 +0900

    test shell script

---
+++ source/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "*/5 * * * * root docker exec biscon /usr/bin/python3 /bis/source/test.py" > /etc/cron.d/cronjob
+
+echo "* * * * * root python /bis/source/test.py > /bis/source/test.log 2>&1" >> /etc/cron.d/cronjob
+
+chmod 0644 /etc/cron.d/cronjob
+
+crontab /etc/cron.d/cronjob
+
+service cron restart
