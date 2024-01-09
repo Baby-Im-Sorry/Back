1f1944847b4a25d5e986d179bbd7b05e074aea3a
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-10 00:49:17 +0900

    Update test.sh

--- source/test.sh
+++ source/test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
-echo "*/5 * * * * root docker exec biscon /usr/bin/python3 /bis/source/test.py" > /etc/cron.d/cronjob
-
-echo "* * * * * root python /bis/source/test.py > /bis/source/test.log 2>&1" >> /etc/cron.d/cronjob
+echo "*/1 * * * * root docker exec biscon /usr/bin/python3 /bis/source/test.py > /bis/source/test.log 2>&1" > /etc/cron.d/cronjob
 
 chmod 0644 /etc/cron.d/cronjob
 
