005d1741b15b58e2e71d5de6a5c6b7dc707f9e04
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-01-12 17:04:17 +0900

    mongoengine을 사용해서 로그인 클래스화

--- source/models.py
+++ source/models.py
@@ -1,24 +1,27 @@
 from pymongo.collection import Collection
 from config_db import db
+from mongoengine import connect, Document, StringField
+
+connect("test")
 
 #user 테이블 불러오기
 user_collection: Collection = db["users"]
 
-def create_user(username: str):
-    user_data = {
-        "username": username,
-    }
-    user_id = user_collection.insert_one(user_data).inserted_id
+class User(Document):
+    username = StringField(required=True, unique=True)
+
+def create_user(req_name: str):
+    new_user = User(username=req_name)
+    user_id = new_user.save().id
     return user_id
 
-def check_user(username: str):
-    existing_user = user_collection.find_one({"username": username})
-
+def check_user(req_name: str):
+    existing_user = User.objects(username=req_name).first()
     if not existing_user: # 없는 유저 -> 회원가입
         try:
-            user_id = create_user(username)
+            user_id = create_user(req_name)
             return {"msg": "signup", "user_id": str(user_id)}
         except Exception as err:
             return err
     if existing_user: # 기존 유저 -> 로그인
-        return {"msg": "login", "user_id": str(existing_user["_id"])}
+        return {"msg": "login", "user_id": str(existing_user.id)}
