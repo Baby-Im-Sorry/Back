e4e72d2b34f8670ff425dccfa72285c35b52e709
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-12 22:58:15 +0900

    Update login endpoint in routers.py

--- source/routers.py
+++ source/routers.py
@@ -1,19 +1,25 @@
 from fastapi import APIRouter, HTTPException
 from models import check_user
 
+from fastapi import Depends, HTTPException, status, Cookie, Form
+
 router = APIRouter()
+
 
 @router.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Team BIS"}
 
-@router.post("/login/{username}")
-def login(username: str):
+
+@router.post("/login")
+async def login(
+    username: str = Form(...),
+):
     try:
         result = check_user(username)
-        if result["msg"] == "signup": # 새 유저 생성됨
+        if result["msg"] == "signup":  # 새 유저 생성됨
             return {"message": "signup", "user_id": str(result["user_id"])}
-        if result["msg"] == "login": # 기존 유저 로그인
+        if result["msg"] == "login":  # 기존 유저 로그인
             return {"message": "login", "user_id": str(result["user_id"])}
     except HTTPException as e:
         return HTTPException(status_code=500, detail=f"Login Error: {str(e)}")
