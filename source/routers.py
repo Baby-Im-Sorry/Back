11e12d868c2196d6593bdd44db3a0ad691b7d270
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-12 22:59:34 +0900

    Refactor login endpoint in routers.py

--- source/routers.py
+++ source/routers.py
@@ -1,19 +1,21 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Form, HTTPException
 from models import check_user
 
 router = APIRouter()
+
 
 @router.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@router.post("/login/{username}")
-def login(username: str):
+
+@router.post("/login")
+def login(username: str = Form(...)):
     try:
         result = check_user(username)
-        if result["msg"] == "signup": # 새 유저 생성됨
+        if result["msg"] == "signup":  # 새 유저 생성됨
             return {"message": "signup", "user_id": str(result["user_id"])}
-        if result["msg"] == "login": # 기존 유저 로그인
+        if result["msg"] == "login":  # 기존 유저 로그인
             return {"message": "login", "user_id": str(result["user_id"])}
     except HTTPException as e:
         return HTTPException(status_code=500, detail=f"Login Error: {str(e)}")
