333edc3480e695629e1f888935fd1346c1b8acc5
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-13 00:49:56 +0900

    Add CORS middleware to FastAPI app

--- source/main.py
+++ source/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from routers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용, 특정 도메인으로 제한 가능
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
