ca68308bee8b235b96461f049abfe5b92d5c5a38
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-13 22:05:33 +0900

    Add save_request and save_breifing functions

--- source/models.py
+++ source/models.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 import certifi  # mongoDB 보안관련 설정
 from dotenv import load_dotenv  # .env 불러오기
 import os
+import datetime
 
 # .env 파일 로드
 dotenv_path = os.path.join(
@@ -19,13 +20,15 @@ DATABASE_URI = DATABASE_URI.replace("<DATABASE_ID>", DATABASE_ID).replace(
     "<DATABASE_PASSWORD>", DATABASE_PASSWORD
 )
 client = MongoClient(DATABASE_URI, tlsCAFile=certifi.where())  # MongoDB 주소와 포트에 맞게 수정
-db = client["test"]  # 데이터베이스 이름에 맞게 수정
+db = client["BIS"]  # 데이터베이스 이름에 맞게 수정
 
 # user 테이블 불러오기
 user_collection: Collection = db["users"]
+request_collection: Collection = db["requests"]
+breifing_collection: Collection = db["breifings"]
 
 
-def create_user(username: str):
+def save_user(username: str):
     user_data = {
         "username": username,
     }
@@ -33,12 +36,33 @@ def create_user(username: str):
     return user_id
 
 
+def save_request(user_id: str, interval: int, endtime: str):
+    # request_name: 요청 이름 -> 그날 날짜 시간으로 자동 저장
+    request_data = {
+        "user_id": user_id,
+        "request_name": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "interval": interval,
+        "endtime": endtime,
+    }
+    request_id = request_collection.insert_one(request_data).inserted_id
+    return request_id
+
+
+def save_breifing(request_id: str, breifing: str):
+    breifing_data = {
+        "request_id": request_id,
+        "breifing": breifing,
+    }
+    breifing_id = breifing_collection.insert_one(breifing_data).inserted_id
+    return breifing_id
+
+
 def check_user(username: str):
     existing_user = user_collection.find_one({"username": username})
 
     if not existing_user:  # 없는 유저 -> 회원가입
         try:
-            user_id = create_user(username)
+            user_id = save_user(username)
             return {"msg": "signup", "user_id": str(user_id)}
         except Exception as err:
             return err
