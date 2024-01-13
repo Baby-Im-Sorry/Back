acfd8579ee36b835a19b09ccb2ed368ba0667ed0
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-14 01:06:46 +0900

    Add cron job for running tests periodically

--- source/run_test.py
+++ source/run_test.py
@@ -6,11 +6,10 @@ formatted_time = current_time.strftime("%I:%M %p")
 
 
 def test(user_id, interval, endtime):
+    cron_job = f'*/{interval} * {formatted_time.split(":")[0]}-{endtime.split(":")[0]} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1'
     subprocess.run(
-        [
-            "echo",
-            f'"*/{interval} * {formatted_time.split(":")[0]}-{endtime.split(":")[0]} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1" > /etc/cron.d/cronjob_{user_id}',
-        ],
+        f'echo "{cron_job}" > /etc/cron.d/cronjob_{user_id}',
+        shell=True,
     )
     subprocess.run(
         ["chmod", f"/etc/cron.d/cronjob_{user_id}"],
