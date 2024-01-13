942e97827147383142e88f5a6e749e6547959b18
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-14 00:51:17 +0900

    Refactor cron job setup in run_test.py

--- source/run_test.py
+++ source/run_test.py
@@ -5,23 +5,33 @@ current_time = datetime.datetime.now().time()
 formatted_time = current_time.strftime("%I:%M %p")
 
 
-# def test(user_id, interval, endtime):
-#     subprocess.run(
-#         [
-#             f'echo "*/{interval} * {formatted_time.split(":")[0]}-{endtime.split(":")[0]} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1" > /etc/cron.d/cronjob_{user_id} && chmod 0644 /etc/cron.d/cronjob_{user_id} && crontab /etc/cron.d/cronjob_{user_id}'
-#         ],
-#     )
-
-
-# def test(user_id, interval, endtime):
-#     subprocess.run(
-#         [
-#             f'echo "*/{interval} * {formatted_time.split(":")[0]}-{endtime.split(":")[0]} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1" > /etc/cron.d/cronjob_{user_id} && chmod 0644 /etc/cron.d/cronjob_{user_id} && crontab /etc/cron.d/cronjob_{user_id}'
-#         ],
-#     )
-
-
 def test(user_id, interval, endtime):
     subprocess.run(
-        [f"echo {interval} || {interval} || endtime"],
+        [
+            "echo",
+            f'"*/{interval} * {formatted_time.split(":")[0]}-{endtime.split(":")[0]} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1" > /etc/cron.d/cronjob_{user_id}',
+        ],
     )
+    subprocess.run(
+        ["chmod", f"/etc/cron.d/cronjob_{user_id}"],
+    )
+    subprocess.run(
+        ["crontab", f"/etc/cron.d/cronjob_{user_id}"],
+    )
+
+
+# def test(user_id, interval, endtime):
+#     subprocess.run(
+#         [f"echo {interval} || {interval} || endtime"],
+#     )
+
+
+# subprocess.run(["echo", f"hello_{formatted_time}"], shell=True)
+# subprocess.run(["echo", "nice"], shell=True)
+# subprocess.run(["echo", "legend"], shell=True)
+
+# result = subprocess.run(
+#     "echo hello; echo world", shell=True, capture_output=True, text=True
+# )
+
+# print(result.stdout)
