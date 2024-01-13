7616d7513ec048a80e124a4c4c1691910310436d
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-14 02:53:45 +0900

    Refactor run_cron function to use username instead of user_id

--- source/run_cron.py
+++ source/run_cron.py
@@ -7,15 +7,15 @@ now_hour = int(now.split(":")[0])
 now_minute = int(now.split(":")[1])
 
 
-def run_cron(user_id, interval, endtime):
+def run_cron(username, interval, endtime):
     end_hour = int(endtime.split(":")[0])
     cron_job = f"*/{interval} {now_hour}-{end_hour} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1"
     subprocess.run(
-        f'echo "{cron_job}" > /etc/cron.d/cronjob_{user_id}',
+        f'echo "{cron_job}" > /etc/cron.d/cronjob_{username}',
         shell=True,
     )
-    subprocess.run(f"chmod 0644 /etc/cron.d/cronjob_{user_id}", shell=True)
-    subprocess.run(f"crontab /etc/cron.d/cronjob_{user_id}", shell=True)
+    subprocess.run(f"chmod 0644 /etc/cron.d/cronjob_{username}", shell=True)
+    subprocess.run(f"crontab /etc/cron.d/cronjob_{username}", shell=True)
 
 
 # subprocess.run(["echo", f"hello_{formatted_time}"], shell=True)
