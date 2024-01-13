8d6ebf98239fdeaa3a4004ca96bc34a0887c0933
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-14 02:22:25 +0900

    Update cron job in run_test.py

--- source/run_test.py
+++ source/run_test.py
@@ -2,11 +2,14 @@ import subprocess
 import datetime
 
 current_time = datetime.datetime.now().time()
-formatted_time = current_time.strftime("%I:%M %p")
+now = current_time.strftime("%H:%M")
+now_hour = int(now.split(":")[0])
+now_minute = int(now.split(":")[1])
 
 
 def test(user_id, interval, endtime):
-    cron_job = f'*/{interval} * {formatted_time.split(":")[0]}-{endtime.split(":")[0]} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1'
+    end_hour = int(endtime.split(":")[0])
+    cron_job = f"*/{interval} {now_hour}-{end_hour} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1"
     subprocess.run(
         f'echo "{cron_job}" > /etc/cron.d/cronjob_{user_id}',
         shell=True,
@@ -24,3 +27,16 @@ def test(user_id, interval, endtime):
 # )
 
 # print(result.stdout)
+
+user_id = "test"
+interval = 10
+endtime = "1:00"
+end_hour = int(endtime.split(":")[0])
+# end_minute = int(endtime.split(":")[1].split(" ")[0])
+
+cron_job = f"*/{interval} {now_hour}-{end_hour} * * * /usr/bin/python3 /bis/source/cron_test.py >> /bis/cron_test.log 2>&1"
+# subprocess.run(
+#     f'echo "{cron_job}" > /etc/cron.d/cronjob_{user_id}',
+#     shell=True,
+# )
+print(f'echo "{cron_job}" > /etc/cron.d/cronjob_{user_id}')
