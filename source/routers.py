67f85e5a9f9ae45b759f14b8ff9819cf7c1888e1
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-13 22:04:52 +0900

    Add save_request function and startBriefing endpoint

--- source/routers.py
+++ source/routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Form, HTTPException
-from models import check_user
+from models import check_user, save_request
+from test import test
 
 router = APIRouter()
 
@@ -19,3 +20,23 @@ def login(username: str = Form(...)):
             return {"message": "login", "user_id": str(result["user_id"])}
     except HTTPException as e:
         return HTTPException(status_code=500, detail=f"Login Error: {str(e)}")
+
+
+@router.post("/startBreifing")
+def startBriefing(
+    user_id: str = Form(...),
+    interval: int = Form(...),
+    endtime: str = Form(...),
+):
+    try:
+        request_id = save_request(user_id, interval, endtime)
+        test.test(user_id, interval, endtime)
+        # 위에건 크론 등록
+        return {"message": "success", "request_id": str(request_id)}
+    except HTTPException as e:
+        return HTTPException(status_code=500, detail=f"Breifing Error: {str(e)}")
+
+
+# @router.post("/sendclient")
+# def send_to_client():
+#     pass
