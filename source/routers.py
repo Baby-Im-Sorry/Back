3216f3ad4c83fefc14d58838307bf64fc698678b
2eey10 <coherentman0103@naver.com>
2024-01-18 16:46:43 +0900

    Update endBriefing - using sed

--- source/routers.py
+++ source/routers.py
@@ -41,7 +41,7 @@ def startBriefing(
         return HTTPException(status_code=500, detail=f"Briefing Error: {str(e)}")
 
 
-docker_path = os.getenv("DOCKER_PATH", "/usr/bin/docker")
+#docker_path = os.getenv("DOCKER_PATH", "/usr/bin/docker")
 
 @router.post("/endBriefing")
 def endBriefing(username: str = Form(...)):
@@ -51,7 +51,10 @@ def endBriefing(username: str = Form(...)):
        
         # 크론 작업 파일 삭제
         subprocess.run(["rm", cronjob_file])
-        # docker container 내부에서 특정 사용자로 로그인 해 크론 작업 등록 취소 
+
+        # 크론탭 파일 주석 처리
+        subprocess.run([f"sed -i /cronjob_{username}/d {cronjob_file}"], shell=True)
+
         # subprocess.run(["docker", "exec", "-u", username, "biscon", "rm", cronjob_file])
 
         return JSONResponse(content={"message": "Briefing removed", "username": username})
