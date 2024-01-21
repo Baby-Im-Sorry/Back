5d113370316096a463b4b0ae34198f86f06d8d78
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-22 03:40:08 +0900

    Refactor inference_pipeline function to use async/await for WebSocket communication

--- source/inference_pipeline.py
+++ source/inference_pipeline.py
@@ -1,19 +1,20 @@
 import models
 import time
-import argparse
+
+# import argparse
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--request_id", type=str, required=True)
-    args = parser.parse_args()
-    return args
+# def parse_args():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--request_id", type=str, required=True)
+#     args = parser.parse_args()
+#     return args
 
 
-def inference_pipeline(Websocket, request_id):
+async def inference_pipeline(websocket, request_id):
     # args = parse_args()
     # TODO: Implement inference pipeline
     temp_str = f"test_{time.time()}"
     models.save_briefing(briefing=temp_str, request_id=request_id)
-    Websocket.send_text(temp_str)
+    await websocket.send_text(temp_str)
     # return temp_str
