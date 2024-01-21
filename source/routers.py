2cfff275385a68aba49e96b45c5ae93ae4578214
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-01-21 20:07:47 +0900

    DB 감지 웹소켓 코드

--- source/routers.py
+++ source/routers.py
@@ -48,16 +48,39 @@ def endBriefing(
     except HTTPException as e:
         return HTTPException(status_code=500, detail=f"Breifing Error: {str(e)}")
 
+# 웹소켓 라우터
+@router.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
+    await websocket.accept()
 
-# @router.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     change_stream = bf_collection.watch()
-#     try:
-#         while True:
-#             change = next(change_stream)
-#             await websocket.send_text("변화 감지!")
-#     except Exception as e:
-#         print(f"Error: {e}")
-#     finally:
-#         await websocket.close()
+    # 해당 사용자의 가장 최신 request_id 찾기
+    latest_request = bf_collection.request.find_one( ###
+        {"username": username},
+        sort=[("request_id", -1)] # -1 : 내림차순 정렬
+    )
+    # requset_id가 없을 경우 에러 처리
+    latest_request_id = latest_request["request_id"] if latest_request else None 
+
+    # Change Streams 파이프라인 설정
+    if latest_request_id:
+         # Change Streams 파이프라인 설정: 특정 request_id를 갖는 새로운 문서만 감지
+        pipeline = [
+            {"$match": {
+                "operationType": "insert",
+                "fullDocument.request_id": latest_request_id
+            }}
+        ]
+        change_stream = bf_collection.watch(pipeline)
+
+        try:
+            while True:
+                change = next(change_stream)
+                # 새로 추가된 데이터의 내용을 웹소켓을 통해 전송
+                new_data = change["fullDocument"]
+                print('------------------------')
+                print(new_data)
+                await websocket.send_text(f"새로운 데이터 추가됨: {new_data}")
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            await websocket.close()
