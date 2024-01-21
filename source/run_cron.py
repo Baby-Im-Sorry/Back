c429ba866b092d6f13eafec8b4e39824c8e5bd13
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2024-01-21 16:35:11 +0900

    Add end_cron function to stop cron job and remove log file

--- source/run_cron.py
+++ source/run_cron.py
@@ -16,15 +16,16 @@ def start_cron(username, interval, endtime):
     subprocess.run(f"crontab /etc/cron.d/cronjob", shell=True)
 
 
-# subprocess.run(["echo", f"hello_{formatted_time}"], shell=True)
-# subprocess.run(["echo", "nice"], shell=True)
-# subprocess.run(["echo", "legend"], shell=True)
+def end_cron(username):
+    subprocess.run(f"rm /bis/cron_{username}.log", shell=True)
+    result = subprocess.run(f"crontab -l", stdout=subprocess.PIPE)
+    crontab_content = result.stdout.decode()
+    cron_jobs = crontab_content.strip().split("\n")
+    for i in cron_jobs:
+        if f"/bis/cron_{username}.log" in i:
+            with open("/etc/cron.d/cronjob", "w") as f:
+                f.write(crontab_content.replace(i, ""))
 
-# result = subprocess.run(
-#     "echo hello; echo world", shell=True, capture_output=True, text=True
-# )
-
-# print(result.stdout)
 
 # user_id = "test"
 # interval = 10
