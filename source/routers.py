ff48603d288608a9da7f59ea1f88bb607814bd30
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-01-23 01:55:32 +0900

    new_request(), watch_db(), websocket_endpoint(), get_latest_request(), endBriefing() 수정

--- source/routers.py
+++ source/routers.py
@@ -45,7 +45,41 @@ def login(username: str = Form(...)):
 #     except HTTPException as e:
 #         return HTTPException(status_code=500, detail=f"Breifing Error: {str(e)}")
 
+def new_request(username, interval, endtime):
+    global scheduler
+    interval = int(interval)
+    request_id = save_request(username, interval, endtime, is_active=True)
+    # 스케쥴러에 작업 등록
+    scheduler = start_scheduler( 
+        username, interval, endtime, scheduler, request_id
+    )
+    return request_id
 
+async def watch_db(request_id, websocket):
+    print('req_id: ',request_id)
+    if request_id != None:
+         # Change Streams 파이프라인 설정: 특정 request_id를 갖는 새로운 문서만 감지
+        try:
+            pipeline = [
+                {"$match": {
+                    "fullDocument.request_id": ObjectId(request_id)
+                }}
+            ]
+            change_stream = bf_collection.watch(pipeline)
+            while True:
+                change = next(change_stream)
+                # 새로 추가된 데이터의 내용을 웹소켓을 통해 전송
+                print('newdata : ------------------------')
+                new_data = change["fullDocument"]['briefing']
+                print(new_data)
+                await websocket.send(f"새로운 데이터 추가됨:{new_data}")
+                print('keep going')
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            await websocket.close()
+
+# DB 감지 코드 (앱 실행 중일 때)
 @router.websocket("/ws")
 async def websocket_endpoint(
     websocket: WebSocket,
@@ -54,49 +88,56 @@ async def websocket_endpoint(
     endtime: str = Query(None),
 ):
     await websocket.accept()
-    global scheduler
-    interval = int(interval)
-    request_id = save_request(username, interval, endtime)
-    scheduler = start_scheduler(
-        username, interval, endtime, scheduler, request_id, websocket
-    )
+    latest_request_id = get_latest_request(username)
 
+    if latest_request_id != None:
+        # request_id의 is_active 확인
+        is_active = rq_collection.find_one({"_id": latest_request_id})["is_active"]
+        if is_active: # 진행중인 브리핑이 있다면, 종료
+            endBriefing(username)
+    # 새로운 요청 생성
+    new_request_id = new_request(username, interval, endtime)
+    # 웹소켓을 통해 지속적인 업데이트를 front에 날려줌.
+    await watch_db(new_request_id, websocket)
+
+# 재접속할 때 DB 불러오기
+@router.get("/getCurrentBriefing")
+async def getCurrentBriefing(username):
+    # latest_request_id = get_latest_request(username)
+    # if latest_request_id:
+    #     # request_id의 is_active 확인
+    #     is_active = rq_collection.find_one({"_id": latest_request_id})["is_active"]
+    #     if is_active: # 진행중인 브리핑이 있다면, 해당 request_id를 갖는 모든 briefing 데이터 찾기
+    #         briefings = bf_collection.find({"request_id": latest_request_id})
+    #         # MongoDB cursor를 리스트로 변환 (비동기 방식이라면 async for 사용)
+    #         briefing_list = []
+    #         for briefing in briefings:
+    #             briefing_list.append(briefing["briefing"])
+    #         print(briefing_list)
+    #         # 결과 반환 & 웹소켓을 통해 지속적인 업데이트를 front 에 날려줌.
+    #         await watch_db(latest_request_id, websocket)
+    #         return {"message": "success", "content": briefing_list}
+    #     else:
+    #         return {"message": "No 'active' request running.", "content": None}
+    return {"getCurrentBriefing"}
+
+# 해당 사용자의 가장 최신 request_id 찾기
+def get_latest_request(username):
+    latest_request = rq_collection.find_one(
+        {"username": username}, sort=[("request_name", -1)]  # -1 : 내림차순 정렬
+    )
+    if latest_request == None:
+        return None
+    return ObjectId(latest_request["_id"])
 
 @router.post("/endBriefing")
 def endBriefing(
     username: str = Form(...),
 ):
+    latest_request_id = get_latest_request(username)
+    rq_collection.update_one({"_id": latest_request_id}, {"$set": {"is_active": False}})
     try:
         end_scheduler(username, scheduler)
         return {"message": "success"}
     except HTTPException as e:
         return HTTPException(status_code=500, detail=f"Breifing Error: {str(e)}")
-
-
-@router.get("/getCurrentBriefing")
-def getCurrentBriefing(username):
-    # 해당 사용자의 가장 최신 request_id 찾기
-    latest_request = rq_collection.find_one(
-        {"username": username}, sort=[("request_name", -1)]  # -1 : 내림차순 정렬
-    )
-    # print(latest_request)
-
-    # request_id가 없을 경우 에러 처리
-    if not latest_request:
-        raise HTTPException(status_code=404, detail="Request not found")
-    latest_request_id = latest_request["_id"]
-    # print(latest_request_id)
-    latest_request_id = ObjectId("65ad4fdc0d7e735ded2fa4be")
-
-    # 해당 request_id를 갖는 모든 briefing 데이터 찾기
-    briefings = bf_collection.find({"request_id": latest_request_id})
-    # print(briefings)
-
-    # MongoDB cursor를 리스트로 변환 (비동기 방식이라면 async for 사용)
-    briefing_list = []
-    for briefing in briefings:
-        briefing_list.append(briefing["briefing"])
-    print(briefing_list)
-
-    # 결과 반환
-    return {"message": "success", "content": briefing_list}
